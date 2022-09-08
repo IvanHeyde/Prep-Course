b4fb0a1ca85d6c46763fa3244e32fe4357590099
IvanHeyde <ivan.heyde.ih@gmail.com>
2022-09-08 14:53:40 -0300

    Ejercicios Completados

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -105,7 +105,18 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-  return arr.sort((a, b) => a.length - b.length)
+  arr.sort(function(a, b){
+    if(a.length > b.length){
+      return 1;
+    }
+    if (a.length < b.length){
+      return -1;
+    }
+    if (a.length === b.length){
+      return 0
+    }
+  });
+  return arr
 }
 
 
